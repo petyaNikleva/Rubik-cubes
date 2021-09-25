52a0c84e70b77f020bc372216f77bcb14d948cca
P N <petyanikleva@yahoo.com>
2021-09-25 13:28:52 +0300

    Add start express app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("started");
+const express = require('express');
+const app = express();
+
+app.all('/', (req, res) => {
+    res.write("It is working!");
+    res.end();
+});
+
+app.listen(5000, () => console.log("Appp is running on http://localhost:5000"));
