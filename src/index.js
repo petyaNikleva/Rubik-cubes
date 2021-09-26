e4507c446bc2c1b843e7e54278a1cf3601edf80d
P N <petyanikleva@yahoo.com>
2021-09-26 16:27:44 +0300

    Add controllers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const initHadlebars = require('./config/handlebars')
 const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 
 initHadlebars(app);
 
 app.use(express.static(path.resolve(__dirname, './public')));
-
-
-app.all('/', (req, res) => {
-    res.render('index')
-});
+app.use(routes);
 
 app.listen(5000, () => console.log("App is running on http://localhost:5000"));
