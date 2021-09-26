aa001db961a3beff73a51a3b87e11aa827f7c7ad
P N <petyanikleva@yahoo.com>
2021-09-26 23:08:35 +0300

    Config enviroment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const initHadlebars = require('./config/handlebars')
 const path = require('path');
 const routes = require('./routes');
+const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
+
 
 const app = express();
 
@@ -13,4 +15,4 @@ app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
 
 
-app.listen(5000, () => console.log("App is running on http://localhost:5000"));
+app.listen(config.PORT, console.log.bind(console, `Application is running on http:localhost:${config.PORT}`));
