8cb74f3a4d72a3ea70bd4c3d145d86c1226e289d
P N <petyanikleva@yahoo.com>
2021-09-26 20:45:52 +0300

    Browse dynamic cubes

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,10 +1,38 @@
+const uniqid = require('uniqid');
+
 class Cube {
+    static #cubes = [
+        {   
+            id:  'i2heiur2uibf',
+            name: 'Mirror Cube',
+            description: 'Some mirror',
+            imageUrl: 'https://m.media-amazon.com/images/I/61xKF56d15L._AC_UX569_.jpg',
+            difficulty: '4'
+          },
+        {   
+            id:  'daadiur2uibfgg',
+            name: 'Ice Cube',
+            description: 'ice singer',
+            imageUrl: 'https://api.time.com/wp-content/uploads/2015/08/ice-cube-straight-outta-compton1.jpg?quality=85&w=1024&h=512&crop=1',
+            difficulty: '1'
+          } 
+    ];
+
     constructor(name, description, imageUrl, difficulty) {
+        this.id = uniqid();
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.difficulty = difficulty;
     }   
+
+    static get cubes() {
+        return Cube.#cubes.slice();
+    }
+
+    static add(cube) {
+        Cube.cubes.push(cube)
+    }
 }
 
 module.exports = Cube;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,21 +1,12 @@
 const Cube = require('../models/Cube');
 
-const cubeDb = [
-    {
-        name: 'Mirror Cube',
-        description: 'Some mirror',
-        imageUrl: 'https://m.media-amazon.com/images/I/61xKF56d15L._AC_UX569_.jpg',
-        difficulty: '4'
-      }
-];
-
-const getAll = () => cubeDb.slice();
+const getAll = () => Cube.cubes;
 
 const create = (name, description, imageUrl, difficulty) => {
 
     let cube = new Cube(name, description, imageUrl, difficulty);
 
-    cubeDb.push(cube);
+    Cube.add(cube);
 };
 
 const cubeService = {
