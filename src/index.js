bcc226ee836fb883cd39753a1ed557c25ae70c8d
P N <petyanikleva@yahoo.com>
2021-09-26 10:05:51 +0300

    Add static assets hosting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const initHadlebars = require('./config/handlebars')
+const path = require('path');
 
 const app = express();
 
 initHadlebars(app);
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
 
 app.all('/', (req, res) => {
     res.render('index')
