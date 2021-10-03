96f3467500150e7c203199fa570ad2c1773a843f
P N <petyanikleva@yahoo.com>
2021-10-03 15:32:27 +0300

    Add "add accessory" function

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -4,6 +4,8 @@ const cubeService = require('../services/cubeService')
 
 const router = express.Router();
 
+const cubeAccessoryController = require('./cubeAccessoryController');
+
 const getCreateCubePage = (req, res) => {
     let cubes = cubeService.getAll();
 
@@ -27,9 +29,15 @@ const cubeDetails  = async (req, res) => {
     res.render('cube/details', {...cube});
 };
 
+// try to do not work now
+// const addCubeAccessory = async (req, res) => {
+//     let cube = await cubeService.getOne(req.params.cubeId);
+//     res.render(`cube/accessory/add`);
+// }
 
 router.get('/create', getCreateCubePage);
 router.post('/create', createCube);
 router.get('/:cubeId', cubeDetails)
+router.use('/:cubeId/accessory', cubeAccessoryController)
 
 module.exports = router;
