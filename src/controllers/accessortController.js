4ee47a2f61421d42880f61f8c25dd80d3b59edf5
P N <petyanikleva@yahoo.com>
2021-10-03 12:24:13 +0300

    add accessory router post functionallity

--- src/controllers/accessortController.js
+++ src/controllers/accessortController.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 router.get('/create', (req, res) => {
      res.render('accessory/create');
+});
+router.post('/create', (req, res) => {
+    let asseccory = req.body;
+    console.log(asseccory);
+    res.redirect('/');
 })
 
 module.exports = router;
