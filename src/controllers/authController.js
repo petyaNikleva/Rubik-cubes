5fd69d82a1c207bfee5f7b52d95e0acab4b2a903
P N <petyanikleva@yahoo.com>
2021-10-14 15:21:01 +0300

    Add register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,4 +4,13 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', (req, res) => {
+     console.log(req.body);
+     res.redirect('/login')
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
 module.exports = router;
