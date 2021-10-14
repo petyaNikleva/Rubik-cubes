3845f4159935d72bcd5f883c050080e8f8e3f73e
P N <petyanikleva@yahoo.com>
2021-10-14 12:44:04 +0300

    Add login form

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,15 @@ const express = require('express');
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 const router = express.Router();
 
 router.use(homeController);
 router.use('/cube', cubeController);
 router.use('/accessory', accessoryController);
+router.use(authController); // here i can use: router.use('/auth', authController);
+
 router.use('*', (req, res) => {
     res.render('404')
 })
